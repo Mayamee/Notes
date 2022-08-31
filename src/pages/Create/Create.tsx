967fe492a3a8fe54d68e961dbdae51c99b95e28e
Mayamee <ponyashcat228@gmail.com>
2022-08-31 11:13:47 +0300

    MaterialUI_course

---
+++ src/pages/Create/Create.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface CreateProps {}
+
+const Create: FunctionComponent<CreateProps> = () => {
+  return <div className="Create">Create page</div>;
+};
+
+export default Create;
---
+++ src/pages/Notes/Notes.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface NotesProps {}
+
+const Notes: FunctionComponent<NotesProps> = () => {
+  return <div className="Notes">Notes page</div>;
+};
+
+export default Notes;
