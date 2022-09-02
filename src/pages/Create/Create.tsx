1a72b45c0cdf919c0d630b597d4f927dba9ab1fc
Mayamee <ponyashcat228@gmail.com>
2022-09-02 12:33:09 +0300

    text fields

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,11 +1,29 @@
-import { FunctionComponent } from "react";
-import { Typography, Button } from "@mui/material";
+import { FunctionComponent, useState } from "react";
+import { Typography, Button, TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import { KeyboardArrowRight } from "@mui/icons-material";
 
 interface CreateProps {}
 
 const Create: FunctionComponent<CreateProps> = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    if (title === "") setTitleError(true);
+
+    if (details === "") setDetailsError(true);
+
+    if (title && details) {
+      console.log({ title, details });
+    }
+  };
   return (
     <div className="Create">
       <Container>
@@ -17,15 +35,38 @@ const Create: FunctionComponent<CreateProps> = () => {
         >
           Create a new Note
         </Typography>
-        <Button
-          type="submit"
-          color="secondary"
-          variant="contained"
-          onClick={() => console.log("you clicked me")}
-          endIcon={<KeyboardArrowRight />}
-        >
-          Submit
-        </Button>
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            onChange={({ target: { value } }) => setTitle(value)}
+            className="gap-input"
+            label="Note title"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+            error={titleError}
+          />
+          <TextField
+            className="gap-input"
+            label="Details"
+            onChange={({ target: { value } }) => setDetails(value)}
+            multiline
+            rows={4}
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+            error={detailsError}
+          />
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            endIcon={<KeyboardArrowRight />}
+          >
+            Submit
+          </Button>
+        </form>
       </Container>
     </div>
   );
