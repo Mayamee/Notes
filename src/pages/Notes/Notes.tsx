08607c29ad68ce65ddbc63a6e04277270291ca5e
Mew <54446552+Mayamee@users.noreply.github.com>
2022-09-03 14:16:49 +0300

    grid system

--- src/pages/Notes/Notes.tsx
+++ src/pages/Notes/Notes.tsx
@@ -1,31 +1,30 @@
 import { FunctionComponent, useEffect, useState } from "react";
 import INoteResponse from "../../models/INoteResponse";
 import NoteService from "../../services/NoteService";
-
+import { Grid, Paper } from "@mui/material";
+import { Container } from "@mui/system";
 interface NotesProps {}
 
 const Notes: FunctionComponent<NotesProps> = () => {
   const [noteList, setNoteList] = useState<INoteResponse[] | null>(null);
   useEffect(() => {
-    NoteService.getAllNotes().then((res) => {
-      setNoteList(res.data);
-      console.log(res.data);
-    });
+    NoteService.getAllNotes().then((res) => setNoteList(res.data));
   }, []);
   return (
     <div className="Notes">
-      <h2>Notes page</h2>
-      {noteList && (
-        <ul>
-          {noteList.map((note, index) => (
-            <li key={`${note.title}-${index}`}>
-              <p>{note.title}</p>
-              <p>{note.details}</p>
-              <p>{note.category}</p>
-            </li>
-          ))}
-        </ul>
-      )}
+      <Container>
+        <h2>Notes page</h2>
+
+        {noteList && (
+          <Grid container>
+            {noteList.map((note, index) => (
+              <Grid item key={`${note.title}-${index}`} xs={12} md={6} lg={4}>
+                <Paper>{note.title}</Paper>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Container>
     </div>
   );
 };
